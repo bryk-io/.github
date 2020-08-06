8f0901b6b12493f99e9054e6dd1754b83c14e514
Ben Cessa <ben@pixative.com>
2020-08-06 00:13:13 -0400

    Publish packages on CI

--- workflow-templates/assets/utils.sh
+++ workflow-templates/assets/utils.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-# Configure git to access private Go modules and install CI bot credentials
-credentials() {
+# Setup runner environment
+setup() {
+  # Configure git to access private Go modules
   mkdir -p 0600 "$HOME"/.ssh
   git config --global url."ssh://git@github.com:".insteadOf "https://github.com"
   git config --global url."ssh://git@bitbucket.org:".insteadOf "https://bitbucket.org"
   ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
   ssh-keyscan -t rsa bitbucket.org >> ~/.ssh/known_hosts
 
+  # Install CI bot credentials
   # The original file must has been previously encrypted:
   # gpg --cipher-algo AES256 -c FILE (interactive)
   # gpg --batch --yes --passphrase="$GPG_PASSPHRASE" --cipher-algo AES256 -c FILE (not interactive)
@@ -22,8 +24,8 @@ credentials() {
 }
 
 case $1 in
-  "credentials")
-  credentials
+  "setup")
+  setup
   ;;
 
   *)
